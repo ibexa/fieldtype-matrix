362983556f0a939db44dcf157c683f7832c2cda1
Michał Grabowski <michal.grabowski@ibexa.co>
2021-12-13 12:08:28 +0100

    IBX-1689: Change all ez- classes/IDs and eZ variables in frontend part

--- src/bundle/Resources/encore/ez.config.manager.js
+++ src/bundle/Resources/encore/ez.config.manager.js
@@ -1,20 +1,20 @@
 const path = require('path');
 
-module.exports = (eZConfig, eZConfigManager) => {
-    eZConfigManager.add({
-        eZConfig,
+module.exports = (ibexaConfig, ibexaConfigManager) => {
+    ibexaConfigManager.add({
+        ibexaConfig,
         entryName: 'ezplatform-admin-ui-content-type-edit-js',
         newItems: [path.resolve(__dirname, '../public/js/scripts/admin.contenttype.matrix.js')],
     });
 
-    eZConfigManager.add({
-        eZConfig,
+    ibexaConfigManager.add({
+        ibexaConfig,
         entryName: 'ezplatform-admin-ui-content-edit-parts-js',
         newItems: [path.resolve(__dirname, '../public/js/scripts/fieldType/ezmatrix.js')],
     });
 
-    eZConfigManager.add({
-        eZConfig,
+    ibexaConfigManager.add({
+        ibexaConfig,
         entryName: 'ezplatform-admin-ui-content-edit-parts-css',
         newItems: [path.resolve(__dirname, '../public/scss/matrix.scss')],
     });
