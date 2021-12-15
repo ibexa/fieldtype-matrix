e1be94ac73a628140d9546375c9d0851ce41c7e4
Michał Grabowski <michal.grabowski@ibexa.co>
2021-12-15 10:51:21 +0100

    ezplatform to ibexa

--- src/bundle/Resources/encore/ez.config.manager.js
+++ src/bundle/Resources/encore/ez.config.manager.js
@@ -3,19 +3,19 @@ const path = require('path');
 module.exports = (ibexaConfig, ibexaConfigManager) => {
     ibexaConfigManager.add({
         ibexaConfig,
-        entryName: 'ezplatform-admin-ui-content-type-edit-js',
+        entryName: 'ibexa-admin-ui-content-type-edit-js',
         newItems: [path.resolve(__dirname, '../public/js/scripts/admin.contenttype.matrix.js')],
     });
 
     ibexaConfigManager.add({
         ibexaConfig,
-        entryName: 'ezplatform-admin-ui-content-edit-parts-js',
+        entryName: 'ibexa-admin-ui-content-edit-parts-js',
         newItems: [path.resolve(__dirname, '../public/js/scripts/fieldType/ezmatrix.js')],
     });
 
     ibexaConfigManager.add({
         ibexaConfig,
-        entryName: 'ezplatform-admin-ui-content-edit-parts-css',
+        entryName: 'ibexa-admin-ui-content-edit-parts-css',
         newItems: [path.resolve(__dirname, '../public/scss/matrix.scss')],
     });
 };
