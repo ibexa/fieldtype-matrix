a59158c70b5c88deb794abb9220930d6e08c25e4
Thorsten Reiter <34274688+reithor@users.noreply.github.com>
2022-06-21 21:44:04 +0200

    IBX-3059_fixed_resolveMatrixFieldValue_args

--- src/lib/GraphQL/FieldValueResolver.php
+++ src/lib/GraphQL/FieldValueResolver.php
@@ -8,17 +8,17 @@ declare(strict_types=1);
 
 namespace Ibexa\FieldTypeMatrix\GraphQL;
 
-use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use Ibexa\GraphQL\Value\Item;
 use Ibexa\FieldTypeMatrix\FieldType\Value\RowsCollection;
 
 class FieldValueResolver
 {
-    public function resolveMatrixFieldValue(Content $content, string $fieldDefIdentifier): RowsCollection
+    public function resolveMatrixFieldValue(Item $item, string $fieldDefIdentifier): RowsCollection
     {
         $silentRows = [];
 
         /** @var \Ibexa\FieldTypeMatrix\FieldType\Value\RowsCollection $rows $rows */
-        $rows = $content->getFieldValue($fieldDefIdentifier)->getRows();
+        $rows = $item->getContent()->getFieldValue($fieldDefIdentifier)->getRows();
         foreach ($rows as $row) {
             $silentRows[] = new SilentRow($row->getCells());
         }
