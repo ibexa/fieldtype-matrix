d04450bcc23d6db11c6f04895c824be777698fdd
Mikolaj Adamczyk <mikolaj.adamczyk@ez.no>
2024-03-15 12:12:57 +0100

    Added test for translation extraction (#40)

---
+++ tests/integration/Kernel.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Integration\FieldTypeMatrix;
+
+use Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle;
+use Ibexa\Bundle\AdminUi\IbexaAdminUiBundle;
+use Ibexa\Bundle\ContentForms\IbexaContentFormsBundle;
+use Ibexa\Bundle\DesignEngine\IbexaDesignEngineBundle;
+use Ibexa\Bundle\FieldTypeMatrix\IbexaFieldTypeMatrixBundle;
+use Ibexa\Bundle\GraphQL\IbexaGraphQLBundle;
+use Ibexa\Bundle\Notifications\IbexaNotificationsBundle;
+use Ibexa\Bundle\Rest\IbexaRestBundle;
+use Ibexa\Bundle\Search\IbexaSearchBundle;
+use Ibexa\Bundle\User\IbexaUserBundle;
+use Ibexa\Contracts\Test\Core\IbexaTestKernel;
+use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
+
+final class Kernel extends IbexaTestKernel
+{
+    public function registerBundles(): iterable
+    {
+        yield from parent::registerBundles();
+
+        yield new LexikJWTAuthenticationBundle();
+        yield new HautelookTemplatedUriBundle();
+        yield new WebpackEncoreBundle();
+        yield new SwiftmailerBundle();
+        yield new KnpMenuBundle();
+
+        yield new IbexaRestBundle();
+        yield new IbexaContentFormsBundle();
+        yield new IbexaSearchBundle();
+        yield new IbexaUserBundle();
+        yield new IbexaDesignEngineBundle();
+        yield new IbexaAdminUiBundle();
+        yield new IbexaNotificationsBundle();
+        yield new IbexaGraphQLBundle();
+
+        yield new IbexaFieldTypeMatrixBundle();
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        parent::registerContainerConfiguration($loader);
+
+        $loader->load(__DIR__ . '/Resources/config.yaml');
+        $loader->load(static function (ContainerBuilder $container): void {
+            $resource = new FileResource(__DIR__ . '/Resources/routing.yaml');
+            $container->addResource($resource);
+            $container->setParameter('form.type_extension.csrf.enabled', false);
+            $container->loadFromExtension('framework', [
+                'router' => [
+                    'resource' => $resource->getResource(),
+                ],
+            ]);
+        });
+    }
+}
